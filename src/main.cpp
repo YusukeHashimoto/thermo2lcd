42022ffb6ac1eadd2723df5a5a60f2d5ab5bb361
Yusuke Hashimoto <yusuke84moto@gmail.com>
2020-09-20 18:48:45 +0900

    リファクタリングおよびサンプルコードのコメント削除

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,11 @@
-/*
-  The circuit:
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 8
- * LCD D4 pin to digital pin 9
- * LCD D5 pin to digital pin 10
- * LCD D6 pin to digital pin 11
- * LCD D7 pin to digital pin 12
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- */
-
-// include the library code:
 #include <LiquidCrystal.h>
 #include <DHT.h>
 
 #define DELAY_TIME 5000
-#define DEBUG_MODE
 
 void rcvThermo(void);
+void dispSerialMonitor(float, float);
+void dispLCD(void);
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
@@ -31,7 +14,6 @@ DHT dht( PIN_DHT, DHT11 );
 
 float temp = 0;
 float humi = 0;
-bool error = false;
 
 void setup() {
   lcd.begin(16, 2);
@@ -45,46 +27,36 @@ void setup() {
 }
 
 void loop() {
+  rcvThermo();
+  dispLCD();
+  dispSerialMonitor(temp, humi);
+  delay(DELAY_TIME);
+}
+
+void rcvThermo(void){
+  const bool isFahrenheit = false;
+  humi = dht.readHumidity();
+  temp = dht.readTemperature(isFahrenheit);
+
+  if (isnan(temp) || isnan(humi)) {
+    Serial.println("ERROR");
+  }
+}
+
+void dispLCD(void) {
   lcd.setCursor(6, 0);
   lcd.print(temp);
 
   lcd.setCursor(6, 1);
   lcd.print(humi);
-
-  rcvThermo();
 }
 
-void rcvThermo(){
-  delay(DELAY_TIME);
-
-  const bool isFahrenheit = false;
-  float percentHumidity = dht.readHumidity();
-  float temperature = dht.readTemperature( isFahrenheit );
-
-  if (isnan(percentHumidity) || isnan(temperature)) {
-    Serial.println("ERROR");
-    Serial.println(percentHumidity);
-    return;
-  }
-
-  float heatIndex = dht.computeHeatIndex(
-    temperature, 
-    percentHumidity, 
-    isFahrenheit);
-
+void dispSerialMonitor(float temp, float humi){
   String s = "Temp: ";
-  s += String(temperature, 1);
+  s += String(temp, 1);
   s += "[C] Humidity: ";
-  s += String(percentHumidity, 1);
-  s += "[%] HI: ";
-  s += String(heatIndex, 1);
+  s += String(humi, 1);
+  s += "[%]";
 
   Serial.println(s);
-
-  temp = temperature;
-  humi = percentHumidity;
-}
-
-void disp_serial_monitor(float temp, float humi){
-  
 }
