d672ec1bbb807828df32164d4c20687b870c1024
Yusuke Hashimoto <yusuke84moto@gmail.com>
2020-09-26 15:24:52 +0900

    SR04を使用して、不在時はバックライトを消灯する機能を実装

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
+#include <SR04.h>
 
 #define DELAY_TIME 5000
 #define USE_DEGREE_CELSIUS false
 
+#define BACKLIGHT_PIN 4
+#define TRIG_PIN 5
+#define ECHO_PIN 6
+
 void rcvThermo(void);
 void dispSerialMonitor(float, float);
 void dispLCD(void);
+void setBackLight(void);
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 const int PIN_DHT = 2;
 DHT dht( PIN_DHT, DHT11 );
+bool backLightOnOff = true;
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long dist;
+int moveLessCount = 0;
 
 float temp = 0;
 float humi = 0;
@@ -25,12 +35,16 @@ void setup() {
   Serial.begin(9600);
   Serial.println("DHT11 start");
   dht.begin();
+
+  pinMode(BACKLIGHT_PIN, OUTPUT);
+  digitalWrite(BACKLIGHT_PIN, HIGH);
 }
 
 void loop() {
   rcvThermo();
   dispLCD();
   dispSerialMonitor(temp, humi);
+  setBackLight();
   delay(DELAY_TIME);
 }
 
@@ -59,4 +73,22 @@ void dispSerialMonitor(float temp, float humi){
   s += "[%]";
 
   Serial.println(s);
+}
+
+void setBackLight(void){
+  long a;
+  a = sr04.Distance();
+  if(abs(a - dist) > 3) {
+    moveLessCount = 0;
+    backLightOnOff = true;
+    dist = a;
+  } else {
+    moveLessCount++;
+    if(moveLessCount > 12) {
+      backLightOnOff = false;
+    }
+  }
+  Serial.println(dist);
+//  backLightOnOff = (a < 20) ? true : false;
+  digitalWrite(BACKLIGHT_PIN, backLightOnOff);
 }
